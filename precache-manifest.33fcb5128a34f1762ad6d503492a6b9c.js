b18d620150a2c0e890e84b482f888b87e98402f9
Lubomir Furinda <l.furinda@gmail.com>
2019-12-21 19:33:07 +0100

    Updates

---
+++ precache-manifest.33fcb5128a34f1762ad6d503492a6b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b68db511e29ba839b9a66dbb22e599",
+    "url": "/ToDoList-React/index.html"
+  },
+  {
+    "revision": "4c4d9abcc18411f716fc",
+    "url": "/ToDoList-React/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6ac8d5fce83eeca09f91",
+    "url": "/ToDoList-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c4d9abcc18411f716fc",
+    "url": "/ToDoList-React/static/js/2.1fa5289f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDoList-React/static/js/2.1fa5289f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ac8d5fce83eeca09f91",
+    "url": "/ToDoList-React/static/js/main.38b4c933.chunk.js"
+  },
+  {
+    "revision": "779115a2e5b55eceae07",
+    "url": "/ToDoList-React/static/js/runtime-main.8054cb68.js"
+  }
+]);
